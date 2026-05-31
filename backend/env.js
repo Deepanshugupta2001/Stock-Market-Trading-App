1693ac87c70b148ccdba8258e7317ff67ad46d1b
Deepanshugupta2001 <deepanshugupta.dg.2001@gmail.com>
2026-05-31 22:28:58 +0530

    Database Update

--- backend/env.js
+++ backend/env.js
@@ -4,7 +4,8 @@ dotenv.config();
 const env = {
     CORS_ORIGIN : process.env.CORS_ORIGIN || "http:localhost:5173",
     PORT : process.env.PORT || 4444,
-    JWT_SECRET : process.env.JWT_SECRET
+    JWT_SECRET : process.env.JWT_SECRET,
+    DATABASE_URL : process.env.DATABASE_URL
 }
 
 export default env;
